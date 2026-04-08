f1db35b29c5d638be987575a01c71f43ce88dda2
Murali Madhav C <madhav@Muralis-MacBook-Pro.local>
2026-04-09 01:31:03 +0530

    Add food types (normal, bonus, slow) with probability-based spawning

--- src/game/food.h
+++ src/game/food.h
@@ -3,8 +3,12 @@
 
 #include "snake.h"
 
+typedef enum { FOOD_NORMAL, FOOD_BONUS, FOOD_SLOW } FoodType;
+
 typedef struct {
     int x, y;
+    FoodType type;
+    int ticks_remaining;   /* 0 = no expiry (normal), >0 = countdown */
 } Food;
 
 Food *food_spawn(Snake *s, int board_w, int board_h, int *seed);
